79c51325c89b1ec51d03bb032f752b7e80ebc0c2
pakeetharan <pakeetharan_b@epiclanka.net>
2022-10-27 00:05:18 +0530

    Initial Commit

---
+++ src/main/java/com/epic/movieapp/util/varlist/MessageVarList.java
@@ -0,0 +1,18 @@
+/**
+ * @date 10/15/2022
+ * @auther Pakeetharan Balasubramaniam
+ **/
+
+package com.epic.movieapp.util.varlist;
+
+public class MessageVarList {
+    //Response Codes
+    public static final String RSP_SUCCESS              = "00";
+    public static final String RSP_NO_DATA_FOUND        = "01";
+    public static final String RSP_NOT_AUTHORISED       = "02";
+    public static final String RSP_TOKEN_EXPIRED        = "03";
+    public static final String RSP_TOKEN_INVALID        = "04";
+    public static final String RSP_ERROR                = "05";
+    public static final String RSP_FAIL                 = "10";
+    public static final String RSP_REQUEST_INVALID      = "06";
+}
